234d388b7e94ee4015e061070d30762e5cfb03cd
Zeba Karin Ahmad <zebakarin@gmail.com>
2022-02-17 23:55:27 +0100

    update test.py

--- Task1_pseudoLabeling/test.py
+++ Task1_pseudoLabeling/test.py
@@ -18,8 +18,16 @@ def test_cifar10(testdataset, filepath = "./path/to/model.pth.tar"):
         with the model file. Assume testdataset is like CIFAR-10. Test this
         function with the testdataset returned by get_cifar10()
     '''
+
     model = torch.load(filepath)
     model.eval()
+
+    inputs, labels = testdataset
+
+    preds = model(inputs)
+    logits = torch.nn.functional.one_hot(preds, num_classes=10)
+
+    return torch.nn.functional.softmax(logits, dim=1)
 
 def test_cifar100(testdataset, filepath="./path/to/model.pth.tar"):
     '''
@@ -36,5 +44,14 @@ def test_cifar100(testdataset, filepath="./path/to/model.pth.tar"):
         with the model file. Assume testdataset is like CIFAR-100. Test this
         function with the testdataset returned by get_cifar100()
     '''
+
     model = torch.load(filepath)
     model.eval()
+
+    inputs, labels = testdataset
+
+    preds = model(inputs)
+    logits = torch.nn.functional.one_hot(preds, num_classes=100)
+
+    return torch.nn.functional.softmax(logits, dim=1)
+    
